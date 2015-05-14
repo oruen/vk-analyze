bec2aadf102c801c069f932d52214d737e296f84
Nick Recobra <oruenu@gmail.com>
2015-05-14 20:38:58 +0300

    Больше данных и изменения в темах группы.

--- get_posts.py
+++ get_posts.py
@@ -4,7 +4,7 @@ import json
 from datetime import datetime
 
 def get_url(offset):
-    return "https://api.vk.com/method/wall.get?domain=teamnavalny&extended=1&filter=all&version=5.31&offset=%s&count=1000" % (offset)
+    return "https://api.vk.com/method/wall.get?domain=teamnavalny&extended=1&filter=all&version=5.31&offset=%s&count=100" % (offset)
 
 def write_header(out):
     data = '\t'.join(['post_id', 'text', 'date', 'num_comments', 'num_likes', 'num_reposts', 'attachment_types'])
@@ -34,9 +34,9 @@ url = get_url(0)
 response = json.loads(urllib2.urlopen(url).read())['response']
 write_header(out)
 save_posts(response, out)
-count = 2250 / 1000
+count = 2250 / 100
 for i in range(1, count + 1):
-    url = get_url(i * 1000)
+    url = get_url(i * 100)
     response = json.loads(urllib2.urlopen(url).read())['response']
     save_posts(response, out)
 out.close()
